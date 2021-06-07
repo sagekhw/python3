680bcdf8c1e9df2bc8b9cda77866825c98022d30
sagekhw <sagekhw@gmail.com>
2021-06-07 14:11:40 +0900

    1

--- app/controller/FileController.py
+++ app/controller/FileController.py
@@ -27,13 +27,14 @@ def a():
 @fileC.route('/fileUpload', methods=['POST'])
 def upload_file():
     print('1')
-    f = request.files['files']
+    #f = request.files['files']
 	#저장할 경로 + 파일명
     print('2')
-    f.save(FPATH + secure_filename(f.filename))
+    #f.save(FPATH + secure_filename(f.filename))
     print('3')
-    files = os.listdir(FPATH)
-    return {'hello':f'{files}'}
+    #files = os.listdir(FPATH)
+    #return {'hello':f'{files}'}
+    return {'hello':'a-simba'}
 
 
 #파일 download 처리
